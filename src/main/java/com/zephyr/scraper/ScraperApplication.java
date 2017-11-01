ecf9eb0174eb8cbef74a0594c558407bdbf41e7f
Oleksandr Melashchenko <oleksandr_melashchenko@epam.com>
2017-11-01 16:55:39 +0200

    Add auto configuration

--- src/main/java/com/zephyr/scraper/ScraperApplication.java
+++ src/main/java/com/zephyr/scraper/ScraperApplication.java
@@ -1,12 +1,20 @@
 package com.zephyr.scraper;
 
+import com.zephyr.scraper.flow.ScrapingFlow;
+import com.zephyr.scraper.flow.impl.ScrapingFlowImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ScraperApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ScraperApplication.class, args);
+    }
+
+    @Bean
+    public ScrapingFlow scrapingFlow() {
+        return new ScrapingFlowImpl();
     }
 }
